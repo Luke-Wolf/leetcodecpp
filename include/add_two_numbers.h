8c44fe98ffd050472b54c84f04c08126b8479854
Luke <lukewolf101010@gmail.com>
2023-10-02 17:56:10 -0700

    add problem comment

--- include/add_two_numbers.h
+++ include/add_two_numbers.h
@@ -25,6 +25,13 @@ class AddTwoNumbers {
 public:
     static auto generateList(std::initializer_list<int> input) -> ListNode *;
 
+    /*
+     * You are given two non-empty linked lists representing two non-negative integers.
+     * The digits are stored in reverse order, and each of their nodes contains a single digit.
+     * Add the two numbers and return the sum as a linked list.
+     *
+     *  You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+     */
     static auto addTwoNumbers(const ListNode *list_1, const ListNode *list_2) -> const ListNode *;
 };
 
